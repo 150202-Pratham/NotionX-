4af3f95b2cfba857c8b0e49bfc1550247ca8afd8
Pratham <prathamgarg1502@gmail.com>
2026-06-19 17:15:15 +0530

    Appended Info to Mail Templates

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -56,7 +56,7 @@ exports.passwordUpdated = (email, name) => {
     <body>
         <div class="container">
             <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-                    src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+                    src="https://res.cloudinary.com/dpdc5d5c0/image/upload/v1781691360/codingOneLineAtATime/pmyfnnh0jn5w3rqyexqb.jpg" alt="StudyNotion Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -66,7 +66,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
+                <a href="mailto:notionx.edu.in@gmail.com"">notionx.edu.in</a>. We are here to help!
             </div>
         </div>
     </body>
--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
       
       <body>
           <div class="container">
-              <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+              <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://res.cloudinary.com/dpdc5d5c0/image/upload/v1781691360/codingOneLineAtATime/pmyfnnh0jn5w3rqyexqb.jpg"
                       alt="StudyNotion Logo"></a>
               <div class="message">Course Payment Confirmation</div>
               <div class="body">
@@ -77,7 +77,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
                   <p>Your Order ID is <b>${orderId}</b></p>
               </div>
               <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                      href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                      href="mailto:notionx.edu.in@gmail.com"">info@notionx.edu.in</a>. We are here to help!</div>
           </div>
       </body>
       
